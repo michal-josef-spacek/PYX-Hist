339255ca4a2de5c70376ee5c81afe0f31a5b466e
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2020-04-24 15:24:46 +0200

    Prepare for 0.06 version

--- Hist.pm
+++ Hist.pm
@@ -8,7 +8,7 @@ use Error::Pure qw(err);
 use List::Util qw(reduce);
 use PYX::Parser;
 
-our $VERSION = 0.05;
+our $VERSION = 0.06;
 
 # Constructor.
 sub new {
@@ -310,6 +310,6 @@ BSD 2-Clause License
 
 =head1 VERSION
 
-0.05
+0.06
 
 =cut
